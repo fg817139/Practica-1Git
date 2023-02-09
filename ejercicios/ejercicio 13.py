91c3cdde9b702088e443fff395c2d0cfa97c6af2
Felipe Gonzalez <agonzalez104@soyudemedellin.edu.co>
2023-02-09 00:49:15 -0500

    se soluciono el proplema del ejercicio 13

---
+++ ejercicios/ejercicio 13.py
@@ -0,0 +1,10 @@
+numero_1= float(input("please enter the first number:"))
+numero_2= float(input("please enter the second number"))
+print(f"resultado de la suma is {numero_1 + numero_2} ")
+print(f"resultado de la resta is {numero_1 - numero_2} ")
+print(f"resultado de la multiplicacion is {numero_1 * numero_2} ")
+print(f"resultado de la division is {numero_1 / numero_2} ")
+
+
+
+
